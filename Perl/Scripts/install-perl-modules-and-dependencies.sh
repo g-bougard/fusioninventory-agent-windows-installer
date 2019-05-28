65eac499ba6c4fbdfae23a1e554582e40d7a148a
Guillaume Bougard <gbougard@teclib.com>
2019-05-28 18:25:30 +0200

    Only use winpcap sdk

--- Perl/Scripts/install-perl-modules-and-dependencies.sh
+++ Perl/Scripts/install-perl-modules-and-dependencies.sh
@@ -137,42 +137,8 @@ fusinv_mod_specific_dependences="$(echo ${fusinv_agent_mod_specific_dependences}
                                  ${tr} '\n' ' ')"
 fusinv_mod_specific_dependences="${fusinv_mod_specific_dependences% *}"
 
-# Download npcap as need to install Net::Pcap perl module for x64
-echo "Downloading Npcap for windows 10..."
-eval ${curl} --silent --location --max-redirs 6 --output "/tmp/${npcap_sdk}" \
-   "${npcap_url}"
-if [ ! -e "/tmp/${npcap_sdk}" ]; then
-   echo "Failed to download netpcap SDK"
-   exit 4
-fi
-# Npcap installation for x64
-arch=x64
-# Extract archive
-eval ${p7z} x -bd -y -o"${strawberry_arch_path}/c" "/tmp/${npcap_sdk}"
-if (( $? == 0 )); then
-   echo "Done and extracted!"
-else
-   echo "Failure!"
-   echo
-   eval echo "There has been an error decompressing \'${npcap_sdk}\'."
-   echo
-   eval echo -n "Perhaps the URL \'${npcap_url}\' is incorrect.\ "
-   echo -n "Please, check the variable '${npcap_url}' in the 'load-perl-environment' "
-   echo "file, and try again."
-   exit 5
-fi
-# Prepare Netpcap libs
-eval ${cp} -avf "${strawberry_arch_path}/c/Lib/${arch}/wpcap.lib"  "${strawberry_arch_path}/c/Lib/wpcap.lib"
-eval ${cp} -avf "${strawberry_arch_path}/c/Lib/${arch}/Packet.lib" "${strawberry_arch_path}/c/Lib/Packet.lib"
-eval ${reimp} --dlltool "${strawberry_arch_path}/c/bin/dlltool.exe" "${strawberry_arch_path}/c/Lib/wpcap.lib"
-eval ${reimp} --dlltool "${strawberry_arch_path}/c/bin/dlltool.exe" "${strawberry_arch_path}/c/Lib/Packet.lib"
-eval ${cp} -avf "libwpcap.a"  "${strawberry_arch_path}/c/Lib/libwpcap.a"
-eval ${cp} -avf "libpacket.a" "${strawberry_arch_path}/c/Lib/libpacket.a"
-eval ${rm} -f "/tmp/${npcap_sdk}" > /dev/null 2>&1
-echo
-
-# Download winpcap as need to install Net::Pcap perl module for x86
-echo "Downloading legacy WinPcap SDK..."
+# Download winpcap as need to install Net::Pcap perl module
+echo "Downloading WinPcap SDK..."
 eval ${curl} --silent --location --max-redirs 6 --output "/tmp/${winpcap_sdk}" \
    "${winpcap_url}"
 if [ ! -e "/tmp/${winpcap_sdk}" ]; then
@@ -180,25 +146,33 @@ if [ ! -e "/tmp/${winpcap_sdk}" ]; then
    exit 6
 fi
 # WinPcap installation for x86
-arch=x86
-# Extract archive
-eval ${p7z} x -bd -y -o"${strawberry_arch_path}/c" "/tmp/${winpcap_sdk}"
-if (( $? == 0 )); then
-   echo "Done and extracted!"
-else
-   echo "Failure!"
-   echo
-   eval echo "There has been an error decompressing \'${winpcap_sdk}\'."
-   echo
-   eval echo -n "Perhaps the URL \'${winpcap_url}\' is incorrect.\ "
-   echo -n "Please, check the variable '${winpcap_url}' in the 'load-perl-environment' "
-   echo "file, and try again."
-   exit 7
-fi
-# Prepare Netpcap libs
-eval ${cp} -avf "${strawberry_arch_path}/c/WpdPack/Lib/libwpcap.a"  "${strawberry_arch_path}/c/Lib/libwpcap.a"
-eval ${cp} -avf "${strawberry_arch_path}/c/WpdPack/Lib/libpacket.a" "${strawberry_arch_path}/c/Lib/libpacket.a"
-eval ${cp} -avf "${strawberry_arch_path}/c/WpdPack/Include/*"  "${strawberry_arch_path}/c/include"
+for arch in ${archs[@]}; do
+   # Extract archive
+   eval ${p7z} x -bd -y -o"${strawberry_arch_path}" "/tmp/${winpcap_sdk}"
+   if (( $? == 0 )); then
+      echo "Done and extracted!"
+   else
+      echo "Failure!"
+      echo
+      eval echo "There has been an error decompressing \'${winpcap_sdk}\'."
+      echo
+      eval echo -n "Perhaps the URL \'${winpcap_url}\' is incorrect.\ "
+      echo -n "Please, check the variable '${winpcap_url}' in the 'load-perl-environment' "
+      echo "file, and try again."
+      exit 7
+   fi
+   # Prepare Netpcap libs
+   if [ "${arch}" == "x64" ]; then
+      eval ${reimp} --dlltool "${strawberry_arch_path}/c/bin/dlltool.exe" "${strawberry_arch_path}/WpdPack/Lib/x64/wpcap.lib"
+      eval ${reimp} --dlltool "${strawberry_arch_path}/c/bin/dlltool.exe" "${strawberry_arch_path}/WpdPack/Lib/x64/Packet.lib"
+      eval ${cp} -avf "libwpcap.a"  "${strawberry_arch_path}/c/Lib/libwpcap.a"
+      eval ${cp} -avf "libpacket.a" "${strawberry_arch_path}/c/Lib/libpacket.a"
+   else
+      eval ${cp} -avf "${strawberry_arch_path}/WpdPack/Lib/libwpcap.a"  "${strawberry_arch_path}/c/Lib/libwpcap.a"
+      eval ${cp} -avf "${strawberry_arch_path}/WpdPack/Lib/libpacket.a" "${strawberry_arch_path}/c/Lib/libpacket.a"
+   fi
+   eval ${cp} -avf "${strawberry_arch_path}/WpdPack/Include/*"  "${strawberry_arch_path}/c/include"
+done
 eval ${rm} -f "/tmp/${winpcap_sdk}" > /dev/null 2>&1
 echo
 
